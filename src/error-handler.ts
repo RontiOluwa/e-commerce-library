2cb9c17036e1fa11b61cd2361197317122915d85
RontiOluwa <olaoluwaojedapo@gmail.com>
2026-02-16 21:14:22 +0100

    feature: added interfaces and cloudinary library

---
+++ src/error-handler.ts
@@ -0,0 +1,16 @@
+import { StatusCodes } from 'http-status-codes';
+
+export interface IErrorResponse {
+    message: string;
+    statusCode: number;
+    status: string;
+    comingFrom: string;
+    serializeErrors(): IError;
+}
+
+export interface IError {
+    message: string;
+    statusCode: number;
+    status: string;
+    comingFrom: string;
+}
